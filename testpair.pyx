c1ebbe1f02d63962b884406a82a0168846c87f28
Peter Tan <pjt554@vdi-n25.nci.org.au>
2020-11-25 21:25:27 +1100

    add tsmask-dask-cython.py

---
+++ testpair.pyx
@@ -0,0 +1,411 @@
+# cython: language_level=3, boundscheck=False
+
+import numpy as np
+
+
+# "cimport" is used to import special compile-time information
+# about the numpy module (this is stored in a file numpy.pxd which is
+# currently part of the Cython distribution).
+cimport numpy as np
+from libc.stdlib cimport malloc, free
+
+
+DTYPEINT = np.int
+DTYPEFL = np.float32
+DTYPEINT8 = np.uint8
+
+ctypedef np.int_t DTYPEINT_t
+ctypedef np.float32_t DTYPEFL_t
+ctypedef np.uint8_t DTYPEINT8_t
+
+cimport cython
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+
+
+cdef float findnbsa( float* vsa, int k, int N, int vss, DTYPEINT8_t* dws_flags, DTYPEINT8_t* vtsmask):
+
+    """
+
+    Function Name: findnbsa
+
+    Description: 
+
+    Given the length of the segment is N, the function will find N clear pixels closest to the segment from the left, N
+    N clear pixels closest to the segment from the right, calculate the mean of these 2N pixels
+
+    Parameters:
+    
+    vsa: float, 1D array
+        time series of mean of surface relectance values
+    k: integer 
+        location of the specified segment
+    vss: integer
+        length of the vsa
+    N: integer
+        length of the time series segment
+    dws_flags: uint8, 1D array
+        flags indicating that a pixel is either a non-shadow pixel or a water pixel
+    vtsmask: uint8, 1D array
+        time series of cloud/shadow labels
+    
+    
+    Return:  mean values of neighbour pixels of the specified segment
+
+ 
+    """
+
+    # clear pixel counter
+    cdef int cc = 0
+
+    # Search direction, 0 -- search the left, 1 -- search the right
+    cdef int dr = 0
+
+    # Directional flags, 0 -- search can be continued, 1 -- search reach the boundary
+    cdef DTYPEINT8_t[:] mvd = np.zeros(2, dtype=np.uint8)
+
+    # location of the left of the segment
+    cdef int lpt = k
+
+    # location of the right of the segment
+    cdef int rpt = k + N - 1
+
+    # sum of the found clear pixels
+    cdef float mid = 0.0
+
+    while cc < 2 * N:
+
+        # search the left
+        if dr == 0:
+            # Not reach the left boundary yet
+            if mvd[0] == 0:
+                while True:
+                    # Move the left pointer to 1 pixel left
+                    lpt -= 1
+                    # reach the begining of the time series?
+                    if lpt < 0:
+                        # Yes, modify the directional flags, change the search directional
+                        mvd[dr] = 1
+                        dr = 1
+                        break
+                    elif vtsmask[lpt] == 1 and dws_flags[lpt] == 1:
+                        # No, if the pixel is a clear pixels and the pixel is not a potenial shadow pxiels
+                        # Add the value of the pixel to the sum of the found clear pixels
+                        mid += vsa[lpt]
+                        # update the clear pixel counter, change the search direction to right
+                        cc += 1
+                        dr = 1
+                        break
+            else:
+                dr = 1
+        else:
+            # search the right
+            if mvd[1] == 0:
+                # Not reach the right boundary yet
+                while True:
+                    # Move the right pointer to 1 pixel right
+                    rpt += 1
+                    # reach the end of the time series?
+                    if rpt == vss:
+                        # Yes, modify the directional flags, change the srach directional
+                        mvd[dr] = 1
+                        dr = 0
+                        break
+                    elif vtsmask[rpt] == 1 and dws_flags[rpt] == 1:
+                        # No, if the pixel is a clear pixels and the pixel is not a potenial shadow pxiels
+                        # Add the value of the pixel to the sum of the found clear pixels
+                        mid += vsa[rpt]
+                        # update the clear pixel counter, change the search direction to left
+                        cc += 1
+                        dr = 0
+                        break
+            else:
+                dr = 0
+
+        # The search reach the boundaries in both direction, exit the search
+        if mvd[0] == 1 and mvd[1] == 1:
+            break
+
+    # if not enough clear pixels found, return 0, otherwise return the mean of the found 2N clear pixels
+    if cc < 2 * N:
+        return 0
+    else:
+        return mid / (2 * N)
+    
+    
+def testpair(float[:] sa, float[:] dwi, int N, DTYPEINT8_t[:] tsmask):
+
+    """
+
+    Function Name: testpair
+
+    Description: 
+    
+    This function identifies cloud and shadow pixels in a time series by comparing its value to its neighbours
+  
+    Parameters:
+    
+    sa: float, 1D array
+        time series of the mean of surface reflectance value of the 6 spectral bands
+    dwi: float, 1D array, 
+        time series of MNDWI (modified normalised water difference index)
+    tsmasak: uint8, 1D array
+        time series of cloud/shadow labels
+    
+
+    Return:
+
+    None, tsmask is updated 
+    
+ 
+    """
+    # cloud detection threshold, the lower the value, the higher cloud detection rate
+    cdef float cspkthd = 0.42
+
+    # shade detection threshold, the lower the value, the higher shade detection rate
+    cdef float sspkthd = 0.42
+
+    # the minimum theshold of a cloud pixel, i.e., all cloud pixels will have a band average
+    # value higher that this theshold
+    cdef float cloudthd = 0.10
+    
+
+    # The shadow pixel theshold
+    cdef float shadowthd = 0.055
+    
+    cdef int tn
+    
+    # length of the time series
+    tn=tsmask.size
+    
+
+    # Find all clear pixels in the time series
+    validx = <int *> malloc(tn*sizeof(int))
+    
+    cdef int i, cc, vss, j, idx
+    
+    cc=0
+    for i in range(tn):
+        if tsmask[i]==1:
+            validx[cc]=i
+            cc=cc+1
+        
+    # Number of valid pixels in the time series
+     
+    vss=cc
+    
+    # Not enough clear pixels in the time series
+    if vss < 3 * N:
+        free(validx)
+        return
+    
+    
+    # Filter out invalid, cloud, shadow points in time series
+    
+    #cdef np.ndarray[DTYPEFL_t, ndim=1] vsa = sa[validx]
+    vsa = <float *> malloc(vss*sizeof(float))
+    
+    #cdef np.ndarray[DTYPEFL_t, ndim=1] vdwi = dwi[validx]
+    vdwi = <float *> malloc(vss*sizeof(float))
+    
+    #cdef np.ndarray[DTYPEINT8_t, ndim=1] vtsmask = tsmask[validx]
+    vtsmask = <DTYPEINT8_t *> malloc(vss*sizeof(char))
+   
+    # flags which indicates if a segment in the time series had been checked 
+    chmarker = <DTYPEINT8_t *> malloc(vss*sizeof(char))
+    
+    # flags which indicates a pixel is either a non-shadow or a water pixels
+    dws_flags = <DTYPEINT8_t *> malloc(vss*sizeof(char))
+    
+    
+    
+    for i in range(vss):
+        idx=validx[i]
+        vsa[i]=sa[idx]
+        vdwi[i]=dwi[idx]
+        vtsmask[i]=tsmask[idx]
+        chmarker[i]=0
+        if vsa[i]>shadowthd or vdwi[i]>0:
+            dws_flags[i]=1
+        else:
+            dws_flags[i]=0
+        
+        
+    
+    
+    
+    
+    # Total number of segments in the time series
+    cdef int numse = vss - N + 1
+
+    # mean values of the time series segments
+    msa = <float *> malloc(numse*sizeof(float))
+    
+    
+    cdef float pms
+    
+    # calculate mean values of the time series segments
+    for i in range(numse):
+        if N == 1:
+            msa[i]=vsa[i]
+        else:
+            pms=0
+            for j in range(i, i+N):
+                pms += vsa[j]
+            msa[i]=pms/N
+    
+    
+ 
+
+    # index array corresponding to descending order of the values of msa array
+    sts = <int *> malloc(numse*sizeof(int))
+   
+
+    cdef float up_bound, cur_max
+    
+    
+    # sort the time series of mean of the segemnts in descending order
+    upbound=1000.0
+    for i in range(numse):
+        cur_max=-1
+        for j in range(numse):
+            upbound = 100.0
+            if (msa[j]> cur_max and msa[j]< upbound):
+                cur_max=msa[j]
+                sts[i]=j
+        upbound=cur_max
+    
+  
+  
+    cdef int k
+    cdef float m1, m2, mid
+  
+    
+    # detect anormalty among time series data
+    for i in range(numse):
+        k=sts[i]
+        if chmarker[k] == 0:
+            # mean of the segment
+            m2 = msa[k]
+            # mean of the neighbouring 2N pixels
+            mid = findnbsa(vsa, k, N, vss, dws_flags, vtsmask)
+
+            # check if the mean of segemnt is significantly different from the neighbouring pixels
+            if m2 > mid and mid > 0:
+                if (m2 - mid) / mid > cspkthd and m2 > cloudthd:
+                    # cloud pixels
+                    for j in range(k, k+N):
+                        vtsmask[j] = 2
+                        chmarker[j] = 1
+
+            elif mid > m2 and m2 > 0:
+                if (mid - m2) / m2 > sspkthd and m2 < shadowthd:
+                    # shadow pixels
+                    for j in range(k, k+N):
+                        vtsmask[j] = 3
+                        chmarker[j] = 1
+                   
+    # update the orginal time series mask with vtsmask
+    
+    for i in range(vss):
+        tsmask[validx[i]] = vtsmask[i]
+    
+    
+    free(validx)
+    free(vsa)
+    free(vdwi)
+    free(vtsmask)
+    free(chmarker)
+    free(dws_flags)
+    free(msa)
+    free(sts)
+
+
+def spatial_buffer(onescene):
+
+    """
+
+    Function Name: 
+
+    Description: 
+    
+    This function labels neighbouring pixels of cloud and shadow pixels as cloud/shadow pixels
+  
+    Parameters:
+    
+    onescene: uint8, 2D array
+        One scene of the tsmask dataarray
+        
+    Return:  
+
+    updated tsmask with cloud/shadow mask values
+ 
+    """
+
+    cdef np.ndarray[DTYPEINT8_t, ndim=2] tsmask, newmask
+    
+    tsmask = onescene
+    newmask = onescene.copy()
+
+    cdef int irow, icol, y, x
+    
+    irow, icol = onescene.shape
+
+    for y in np.arange(irow - 2):
+        for x in np.arange(icol - 2):
+            block = tsmask[y : y + 3, x : x + 3]
+            # if the center pixel in the block is a cloud or shadow
+            if block[1, 1] == 2 or block[1, 1] == 3:
+                # label neighbouring pixels as cloud/shadow pixels
+                newmask[y : y + 3, x : x + 3] = block[1, 1]
+
+    # Only change clear pixels' labels
+    vidx = tsmask == 1
+    tsmask[vidx] = newmask[vidx]
+
+    return tsmask
+
+
+def spatial_filter(onescene):
+
+    """
+
+    Function Name: 
+
+    Description: 
+    
+    This function labels cloud and shadow pixels with less than M surrounding cloud/shadow pixels as clear pixels
+  
+    Parameters:
+    
+    onescene: uint8, 2D array
+        One scene of the tsmask dataarray
+        
+    Return:  
+    
+    updated tsmask with cloud/shadow mask values
+ 
+    """
+    cdef np.ndarray[DTYPEINT8_t, ndim=2] tsmask, block
+    
+    tsmask = onescene
+    
+    cdef int M = 2
+
+    cdef int irow, icol, y, x
+    
+    irow, icol = onescene.shape
+    for y in np.arange(irow - 2):
+        for x in np.arange(icol - 2):
+            block = tsmask[y : y + 3, x : x + 3]
+            # if the center pixel in the block is a cloud or shadow
+            if block[1, 1] == 2 or block[1, 1] == 3:
+                # if total number of cloud/shadow pixels in the block is less than M+1,
+                # label the center pixel as a clear pixel
+
+                if np.logical_or(block == 2, block == 3).sum() < M + 1:
+                    tsmask[y + 1, x + 1] = 1
+
+    return tsmask
+
---
+++ tsmask-dask-cython.py
@@ -0,0 +1,528 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import rasterio
+import xarray as xr
+import sys
+import datacube
+import dea_datahandling as ddh
+import numpy as np
+import tsmask_func as tsf
+from multiprocessing import Pool
+import dask.array as da
+import testpair as cym
+
+
+# In[2]:
+
+
+def load_s2_nbart_ts_cor_dask(
+    dc, lat_top, lat_bottom, lon_left, lon_right, start_of_epoch, end_of_epoch, chunks, cor_type
+):
+
+    
+    allbands = [
+        "nbart_blue",
+        "nbart_green",
+        "nbart_red",
+        "nbart_nir_2",
+        "nbart_swir_2",
+        "nbart_swir_3",
+        "fmask",
+    ]
+    
+    # Define spatial and temporal coverage
+
+    if (cor_type==0):
+        newquery = {
+        "crs": "EPSG:3577",
+        "x": (lon_left, lon_right),
+        "y": (lat_top, lat_bottom),
+        "time": (start_of_epoch, end_of_epoch),
+        "output_crs": "EPSG:3577",
+        "resolution": (-20, 20),
+        "measurements": allbands,
+        "dask_chunks": chunks,
+        "group_by": "solar_day",
+        }
+    elif (cor_type==1):
+        newquery = {
+        "x": (lon_left, lon_right),
+        "y": (lat_top, lat_bottom),
+        "time": (start_of_epoch, end_of_epoch),
+        "output_crs": "EPSG:3577",
+        "resolution": (-20, 20),
+        "measurements": allbands,
+        "dask_chunks": chunks,
+        "group_by": "solar_day",
+        }
+
+    # Names of targeted spectral bands
+
+
+    # Band names used with in the dataset
+    new_bandlabels = ["blue", "green", "red", "nir", "swir1", "swir2", "fmask"]
+
+    # Load S2 data using Datacube API
+ 
+
+    s2_ds = ddh.load_ard(dc,
+             products=['s2a_ard_granule', 's2b_ard_granule'],
+             min_gooddata=0.0,
+             mask_pixel_quality=False,
+             **newquery)
+    
+    # Rename spectral band names to new band labels
+    rndic = dict(zip(allbands, new_bandlabels))
+    s2_ds = s2_ds.rename(rndic)
+
+    # Add tsmask dataarray to the dataset
+
+    s2_ds["tsmask"] = s2_ds["blue"]
+    s2_ds["tsmask"].values = np.zeros(
+        (s2_ds["time"].size, s2_ds["y"].size, s2_ds["x"].size), dtype=np.uint8)
+        
+    
+    return s2_ds
+
+
+
+
+
+def tsmask_filter_block(s2_ds, block):
+    
+    # Define the spatial subset
+    
+    [r1, r2, c1, c2] = block
+    print("Loading S2 NBART time series surface reflectance data for block(", r1, r2, c1, c2, ")")
+    
+    # scale factor
+    scale = 10000.0
+
+    # invalid value
+    ivd = -999
+    
+    
+    # Load NABR-T data from Dask array
+    
+   
+    blue = s2_ds["blue"][:, r1:r2, c1:c2].values
+    print("Finish loading the blue band")
+    green = s2_ds["green"][:, r1:r2, c1:c2].values
+    print("Finish loading the green band")
+    red = s2_ds["red"][:, r1:r2, c1:c2].values
+    print("Finish loading the red band")
+    nir = s2_ds["nir"][:, r1:r2, c1:c2].values
+    print("Finish loading the nir band")
+    swir1 = s2_ds["swir1"][:, r1:r2, c1:c2].values
+    print("Finish loading the swir1 band")
+    swir2 = s2_ds["swir2"][:, r1:r2, c1:c2].values
+    print("Finish loading the swir2 band")
+    tsmask = s2_ds["tsmask"][:, r1:r2, c1:c2].values
+    print("Finish loading the tsmask band")
+
+    
+    # initialise tsmask, all as clear pixels
+    tsmask[:] = 1
+
+
+ 
+    # detect pixels with invalid data value
+    tsmask[blue<=ivd]=0
+    tsmask[green<=ivd]=0
+    tsmask[red<=ivd]=0
+    tsmask[nir<=ivd]=0
+    tsmask[swir1<=ivd]=0
+    tsmask[swir2<=ivd]=0
+                    
+    # detect pixels with invalid data value  
+    tsmask[blue == 0] = 0
+    tsmask[green == 0] = 0
+    tsmask[red == 0] = 0
+    tsmask[nir == 0] = 0
+    tsmask[swir1 == 0] = 0
+    tsmask[swir2 == 0] = 0
+
+    print("Start indices calculation")
+        
+    #Band average, a proxy index of brightness
+    sa = ((blue+green+red+nir+swir1+swir2)/scale/6).astype(np.float32)
+   
+    
+    # modified normalised difference water index
+    mndwi = ((green - swir1) / (green + swir1)).astype(np.float32)
+  
+    
+    # modified soil adjusted vegetation index
+    msavi = ((2 * nir/scale + 1 - np.sqrt((2 * nir/scale + 1) * (2 * nir/scale + 1) - 8 * (nir/scale - red/scale))) / 2).astype(np.float32)
+    
+
+
+    # Band different ratio between red and blue
+    wbi = ((red - blue) / blue).astype(np.float32)
+   
+    
+    # Sum of red and blue band
+    rgm = ((red + blue)/scale).astype(np.float32)
+    
+    
+    
+    # Band different ratio between green and mean of red and blue
+    grbm = (green - (red + blue) / 2) / ((red + blue) / 2).astype(np.float32)
+      
+    
+    # Bright cloud theshold
+    maxcldthd = 0.45
+
+    # label all ultra-bright pixels as clouds
+    tsmask[sa > maxcldthd] = 2
+    
+    
+    
+    # Prepare tuples as input of multiprocessing 
+    ts_tuples=create_ts_tuples_direct(sa, mndwi, msavi, wbi, rgm, grbm, tsmask)
+    
+    results = []
+    
+    # number of process for the  pool object
+    number_of_workers = 14
+    
+    # Create a Pool object with a number of processes
+    p = Pool(number_of_workers)
+    
+    print("Runing time series cloud and shadow detection for block(", r1, r2, c1, c2, ")")
+    # Start runing the cloud detection function using a pool of independent processes
+    results = p.starmap(perpixel_filter_direct_v2, ts_tuples)    
+    
+    # Finish the parallel runs
+    p.close()
+    
+    # Join the results and put them back in the correct order
+    p.join()
+
+ 
+   # number of rows
+    irow = tsmask.shape[1]
+
+    # number of columns
+    icol = tsmask.shape[2]
+    
+    #number of time slice
+    
+    tn = s2_ds["time"].size
+ 
+    #for y in np.arange(irow):
+    #    for x in np.arange(icol):
+    #        s2_ds["tsmask"][:, r1+y, c1+x].values = results[y * icol + x]
+
+    s2_ds["tsmask"][:, r1:r2, c1:c2].values = np.array(results).transpose().reshape(tn, irow, icol).copy()
+    
+
+     
+    del ts_tuples
+    del results
+    del sa
+    del mndwi 
+    del msavi 
+    del wbi 
+    del rgm 
+    del grbm 
+    
+    
+
+    
+    return 
+    
+   
+    
+    
+
+
+# In[6]:
+
+
+def create_ts_tuples_direct(sa, mndwi, msavi, wbi, rgm, grbm, tsmask):
+
+    """
+
+    Function Name: create_ts_tuples
+
+    Description: 
+    
+    This function creates a list of tuples of Sentinel-2 surface reflectance data, the list will 
+    serve as the input when the Multiprocessing Pool method is called 
+
+  
+    Parameters: 
+    
+    s2_ds: Xarray dataset Object
+        the dataset containing dataarrays of time series surface reflectance data
+        
+    Return: 
+    
+    a list of tuples of Sentinel-2 surface reflectance data
+
+ 
+    """
+    # number of rows
+    irow = tsmask.shape[1]
+
+    # number of columns
+    icol = tsmask.shape[2]
+
+    # total number of pixels
+    pnum = irow * icol
+
+    ts_tuples = []
+
+    for i in np.arange(pnum):
+
+        y = int(i / icol)
+        x = i % icol
+
+        # copy time series spectral data from the data set, scale the data to float32, in range (0, 1.0)
+
+        ts_tuples.append((sa[:, y, x], mndwi[:, y, x], msavi[:, y, x], wbi[:, y, x], rgm[:, y, x], grbm[:, y, x], tsmask[:, y, x]))
+
+    return ts_tuples
+
+
+# In[7]:
+
+
+def perpixel_filter_direct_v2(sa, mndwi, msavi, wbi, rgm, grbm, tsmask):
+
+    """
+
+    Function Name: perpixel_filter_direct
+
+    Description: 
+    
+    This function performs time series cloud/shadow detection for one pixel
+  
+    Parameters: 
+    
+    blue, green, red, nir, swir1, swir2: float, 1D arrays
+        Surface reflectance time series data of band blue, green, red, nir, swir1, swir2 for the pixel
+        
+    tsmask: float, 1D array
+        Cloud /shadow mask time series for the pixel
+    
+    Return:  
+    
+    Updated cloud/shadow mask time serie
+
+ 
+    """
+
+    # detect single cloud / shadow pixels
+    cym.testpair(sa, mndwi, 1, tsmask)
+    cym.testpair(sa, mndwi, 1, tsmask)
+    cym.testpair(sa, mndwi, 1, tsmask)
+
+    # detect 2 consecutive cloud / shadow pixels
+    cym.testpair(sa, mndwi, 2, tsmask)
+    cym.testpair(sa, mndwi, 2, tsmask)
+
+    # detect 3 consecutive cloud / shadow pixels
+    cym.testpair(sa, mndwi, 3, tsmask)
+
+    # detect single cloud / shadow pixels
+    cym.testpair(sa, mndwi, 1, tsmask)
+
+    # cloud shadow theshold
+    shdthd = 0.05
+
+    # mndwi water pixel theshold
+    dwithd = -0.05
+
+    # mndwi baregroud pixel theshold
+    landcloudthd = -0.38
+
+    # msavi water pixel theshold
+    avithd = 0.06
+
+    # mndwi water pixel theshold
+    wtdthd = -0.2
+
+    for i, lab in enumerate(tsmask):
+
+        if lab == 3 and mndwi[i] > dwithd and sa[i] < shdthd:  # water pixel, not shadow
+            tsmask[i] = 1
+
+        if lab == 2 and mndwi[i] < landcloudthd:  # bare ground, not cloud
+            tsmask[i] = 1
+
+        if (
+            lab == 3 and msavi[i] < avithd and mndwi[i] > wtdthd
+        ):  # water pixel, not shadow
+            tsmask[i] = 1
+
+        if (
+            lab == 1
+            and wbi[i] < -0.02
+            and rgm[i] > 0.06
+            and rgm[i] < 0.29
+            and mndwi[i] < -0.1
+            and grbm[i] < 0.2
+        ):  # thin cloud
+            tsmask[i] = 2
+
+    return tsmask
+
+
+
+# This functioin divides the dataset into a list of blocks with smaller spatial dimensions 
+
+def create_blocks(irow, icol, my, mx):
+    
+    zy=int(irow/my)+1
+    zx=int(icol/mx)+1
+    
+    
+    blist=[]
+    
+    for i in np.arange(zy):
+        r1=i*my
+        r2=r1+my
+        if r2>irow:
+            r2=irow
+        for j in np.arange(zx):
+            oneblock=np.zeros(4, dtype=int)
+            c1=j*mx
+            c2=c1+mx
+            if (c2>icol):
+                c2=icol
+            
+            blist.append(np.array([r1, r2, c1, c2]))
+            
+    return blist
+
+def create_blocks_v2(irow, icol, ss):
+    
+   
+    nrow=ss//icol +1
+    
+    zy=irow//nrow +1
+    
+    blist=[]
+
+    
+    for i in np.arange(zy):
+        r1=i*nrow
+        r2=r1+nrow
+        if r2>irow:
+            r2=irow
+       
+        blist.append(np.array([r1, r2, 0, icol]))
+            
+    return blist
+
+
+start_of_epoch, end_of_epoch = "2015-01-01", "2020-12-31"
+#simpson desert
+#(y1, y2, x1, x2) =  (-2682355.0, -2725625.0, 438225.0, 469355.0)
+
+#Tile 55HGU
+#(y1, y2, x1, x2) =  (-36.99497079, -38.01366742, 149.24791461, 150.52675786)
+
+
+#Tile 55JGK
+#(y1, y2, x1, x2) = (-27.087734,  -28.09861708, 149.01710236, 150.15181468)
+(y1, y2, x1, x2) = (-27.087734,  -27.59315, 149.01710236, 149.5844)
+
+
+dc = datacube.Datacube(app='load_clearsentinel')
+
+tg_ds=load_s2_nbart_ts_cor_dask(dc, y1, y2, x1, x2, start_of_epoch, end_of_epoch, {   
+        "time": 1,
+    }, 1   )
+
+
+
+
+irow=tg_ds['y'].size
+icol=tg_ds['x'].size
+tn = tg_ds['time'].size
+
+mem=128
+
+#my=800
+#mx=800
+#blist=create_blocks(irow, icol, my, mx)
+print(irow, icol, tn)
+
+ss=int(720000*2.1*550*mem/tn/64)
+
+blist=create_blocks_v2(irow, icol, ss)
+
+for block in blist:
+      
+    tsmask_filter_block(tg_ds, block)
+
+
+
+
+results = []
+
+# number of process for the  pool object
+number_of_workers = 14
+
+# Create a Pool object with a number of processes
+p = Pool(number_of_workers)
+
+# create a list of scene
+paralist = [tg_ds["tsmask"].values[i, :, :] for i in np.arange(tg_ds.time.size)]
+
+# Start runing the spatial filter function using a pool of indepedent processes
+results = p.map(cym.spatial_filter, paralist)
+
+
+# Finish the parallel runs
+p.close()
+
+# Join the results and put them back in the correct order
+p.join()
+
+
+# Save the cloud/shadow masks to the 'tsmask' dataarray in the s2_ds dataset
+for i in np.arange(tg_ds.time.size):
+    tg_ds["tsmask"].values[i, :, :] = results[i]
+    
+
+#output the tsmask to as cloud optimised geotiff files
+bandsets=['tsmask']
+outbandnames=['tsmask']
+#dirc='/g/data/u46/pjt554/tsmask_validation_data/simpson'
+#loc_str='simpson'
+dirc='/g/data/u46/pjt554/tsmask_validation_data/55JGK'
+loc_str='55JGK'
+tsf.output_ds_to_cog(bandsets, outbandnames, dirc, loc_str, tg_ds)
+
+
+
+# check if the output file is correct
+#testfile=dirc+'/simpson_tsmask_2017-10-10.tif'
+#onemask=rasterio.open(testfile)
+#aa = onemask.read()
+#print(onemask.bounds)
+#print(onemask.crs)
+#onemask.transform*(0,0)
+
+
+
+
+
+print(tg_ds['tsmask'][:,53, 3])
+
+print(tg_ds['tsmask'][:, 53, 53])
+
+
+
+
+
+
+
