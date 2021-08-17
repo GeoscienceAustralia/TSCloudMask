0779e70c6ce0eda08a58d3f5ad60d86b6c2b4e5b
pjt554 <peter.tan@ga.gov.au>
2021-08-17 02:37:16 +0000

    rename module name

--- nmask_ancillary/setup.py
+++ nmask_ancillary/setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 import numpy
 
 setuptools.setup(
-    name = "nmask-ancillary",
+    name = "nmask_ancillary",
     ext_modules=cythonize("nmask_cmod.pyx"),
     include_dirs=[numpy.get_include()]
 )
